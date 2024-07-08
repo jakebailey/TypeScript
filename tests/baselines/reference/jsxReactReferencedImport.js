0a45b40b3af255f332dbd9b57591ced65fa4a74d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-07-08 09:37:13 -0700

    Add test

---
+++ tests/baselines/reference/jsxReactReferencedImport.js
@@ -0,0 +1,18 @@
+//// [tests/cases/conformance/jsx/jsxReactReferencedImport.ts] ////
+
+//// [file.tsx]
+/// <reference path="/.lib/react16.d.ts" />
+
+import React from 'react';
+
+export function Component1() {
+  return <div />;
+}
+
+
+//// [file.js]
+/// <reference path="react16.d.ts" />
+import React from 'react';
+export function Component1() {
+    return React.createElement("div", null);
+}
---
+++ tests/cases/conformance/jsx/jsxReactReferencedImport.ts
@@ -0,0 +1,15 @@
+// @filename: file.tsx
+// @jsx: react
+// @module: esnext
+// @allowSyntheticDefaultImports: true
+// @noUnusedLocals: true
+// @noUnusedParameters: true
+// @verbatimModuleSyntax: true
+
+/// <reference path="/.lib/react16.d.ts" />
+
+import React from 'react';
+
+export function Component1() {
+  return <div />;
+}
