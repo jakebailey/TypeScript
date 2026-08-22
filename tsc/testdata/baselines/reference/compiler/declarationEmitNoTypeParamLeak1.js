8b7122ce93c5b54bc8520c46c071a3010764b9dd
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-22 16:59:31 -0700

    Add declaration emit type parameter leak test
    Ports legacy commits 50bb5242890 (initial test) and 7960372c179 (completed reproducer) as one current-format regression test.

---
+++ tsc/testdata/baselines/reference/compiler/declarationEmitNoTypeParamLeak1.js
@@ -0,0 +1,26 @@
+//// [tests/cases/compiler/declarationEmitNoTypeParamLeak1.ts] ////
+
+//// [declarationEmitNoTypeParamLeak1.ts]
+type BrokenType<T> = "a" | "b";
+
+class MyClass {
+    constructor(readonly arg?: BrokenType<any>, prop = [null as any as BrokenType<any>][0]) {}
+}
+
+
+//// [declarationEmitNoTypeParamLeak1.js]
+"use strict";
+class MyClass {
+    arg;
+    constructor(arg, prop = [null][0]) {
+        this.arg = arg;
+    }
+}
+
+
+//// [declarationEmitNoTypeParamLeak1.d.ts]
+type BrokenType<T> = "a" | "b";
+declare class MyClass {
+    readonly arg?: BrokenType<any> | undefined;
+    constructor(arg?: BrokenType<any> | undefined, prop?: BrokenType<any>);
+}
