07997e59c8291ed18ac9b0a2aa7ea4c7fa0bbf98
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-22 17:42:10 -0700

    Test template literal union reduction

--- tsc/internal/checker/checker_test.go
+++ tsc/internal/checker/checker_test.go
@@ -1,6 +1,8 @@
 package checker_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/microsoft/TypeScript/tsc/internal/ast"
@@ -16,10 +18,10 @@ func TestGetSymbolAtLocation(t *testing.T) {
 	t.Parallel()
 
 	content := `interface Foo {
-  bar: string;
-}
-declare const foo: Foo;
-foo.bar;`
+	  bar: string;
+	}
+	declare const foo: Foo;
+	foo.bar;`
 	fs := vfstest.FromMap(map[string]string{
 		"/foo.ts": content,
 		"/tsconfig.json": `
@@ -54,5 +56,38 @@ foo.bar;`
 		if symbol == nil {
 			t.Fatalf("Expected symbol to be non-nil")
 		}
+	}
+}
+
+func BenchmarkTemplateLiteralUnionReduction(b *testing.B) {
+	const count = 500
+	var source strings.Builder
+	source.WriteString("type T =\n")
+	for i := range count {
+		fmt.Fprintf(&source, "    | \"common-prefix-%03d-value-common-suffix-%03d\"\n", i, i)
+	}
+	for i := range count {
+		fmt.Fprintf(&source, "    | `common-prefix-%03d-${string}-common-suffix-%03d`\n", i, i)
+	}
+
+	fs := vfstest.FromMap(map[string]string{"/index.ts": source.String()}, true)
+	options := core.CompilerOptions{NoLib: core.TSTrue}
+	programOptions := compiler.ProgramOptions{
+		Config: &tsoptions.ParsedCommandLine{
+			ParsedConfig: &tsoptions.ParsedOptions{
+				FileNames:       []string{"/index.ts"},
+				CompilerOptions: &options,
+			},
+		},
+		Host: compiler.NewCompilerHost("/", fs, bundled.LibPath(), nil, nil, nil),
+	}
+
+	b.ResetTimer()
+	for b.Loop() {
+		program := compiler.NewProgram(programOptions)
+		program.BindSourceFiles()
+		checker, done := program.GetTypeChecker(b.Context())
+		checker.GetDiagnostics(b.Context(), program.GetSourceFile("/index.ts"))
+		done()
 	}
 }
